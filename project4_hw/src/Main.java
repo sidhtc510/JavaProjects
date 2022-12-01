87925e499c4e308b834cbde9fc38a3c2d65f127b
sidhtc510 <sidhtc510@gmail.com>
2022-12-01 13:35:49 +0100

    .

---
+++ project4_hw/src/Main.java
@@ -0,0 +1,13 @@
+    /*
+    1) Работа со String
+    2) Продемонстировать явное и неявное приведение типов
+    3) Показать переполнение
+    4*) Показать работу с методами и *классами
+    5**) Сделать реверс строки
+     */
+    
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
