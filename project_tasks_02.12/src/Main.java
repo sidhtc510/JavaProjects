9447091347cb5803b5a9449e2ac0d23b0c0d43c9
sidhtc510 <sidhtc510@gmail.com>
2022-12-02 12:46:48 +0100

    .

--- project_tasks_02.12/src/Main.java
+++ project_tasks_02.12/src/Main.java
@@ -2,29 +2,46 @@ import java.util.Scanner;
 
 public class Main {
     static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-      minim();
-    }
 
-    static public void minim(){
+    public static void main(String[] args) {
         System.out.println("enter num 1");
         int a = scanner.nextInt();
         System.out.println("enter num 2");
         int b = scanner.nextInt();
         System.out.println("enter num 3");
         int c = scanner.nextInt();
-       int min;
-        if (a < b){
-            min = a;
-            
-            System.out.println(a);
-        }else{
-            min = b;
-            System.out.println(b);
+
+        // minim(a,b,c);
+        System.out.println(findMin(a, b, c));
+    }
+
+    // static public int minim(int a, int b, int c) {
+    //     int min;
+    //     if (a < b) {
+    //         min = a;
+    //         return a;
+    //     } else {
+    //         min = b;
+    //         return b;
+    //     }
+    //     if(min > c){
+    //         min = c;
+    //         return c;
+    //     }
+    // }
+
+    static int findMin(int a, int b, int c) {
+        // 4. Найти меньшее значение из 3
+        if (a <= b && a <= c) {
+            return a; // 5. Вернуть это значение
+        } else if (b <= c) {
+            return b; // 5. Вернуть это значение
+        } else {
+            return c; // 5. Вернуть это значение
         }
-        
-        if (min > c){
-            min = c;
-        }
+    }
+
+    static int findMin2(int a, int b, int c) {
+        return Math.min(Math.min(a, b), Math.min(b, c));
     }
 }
