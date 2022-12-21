b19d851b458c0a3be5a4f57645b9d0905ba6df03
sidhtc510 <sidhtc510@gmail.com>
2022-12-21 17:48:29 +0100

    .

--- project9_10_HW/src/App.java
+++ project9_10_HW/src/App.java
@@ -9,25 +9,18 @@ import java.util.Scanner;
     4) Инициализировать массив псевдослучайными числами(Random)
     5) Найти сумму и произведение всех элементов этого массива
     6) Сделать класс, который будет искать площадь треугольника, прямоугольника и окружности
-     */
+     7) В классе Human добавить возможность изменения значений
+     8) Написать 15 тернарных операторов
+    9) Показать работу с методами классов String, StringBuilder и StringBuffer
 
-/*
-    1) В классе Human добавить возможность изменения значений
-    *
-    2)Реализовать класс для работы с массивом типа double
-    3) Реализовать класс для работы с массивом типа Human
-    4) Реализовать стирание и удаление, показать разницу между ними
-    5) Реализовать возможность игры человека с человеком в "Камень, ножницы, бумага"
-    6) Предусмотреть оповещение о исчерпании выделенной памяти и недопущение выхода за границы массива
-    7) Реализовать динамическое выделение памяти при ее завершении
-    8) Предусмотреть корректный ввод данных в массив
-    9) Реализовать сортировку массива
-    10) Реализовать поиск в массиве индекса указанного элемента
-     */
+    */
 
 public class App {
 
     static Scanner scanner = new Scanner(System.in);
+    static Human human = new Human(20, "Anatolii");
+
+    static StringBuffer strbfr = new StringBuffer();
 
     public static void main(String[] args) throws Exception {
         // Task 1
@@ -45,11 +38,66 @@ public class App {
         // printArray(arr);
         // sumProizved(arr);
 
-        //Task 6
-        Square sqr = new Square();
-        System.out.println(sqr.pryamougolnik(6, 4));
-        System.out.println(sqr.treugolnik(8, 12));
-        System.out.println(sqr.krug(6));
+        // Task 6
+        // Square sqr = new Square();
+        // System.out.println(sqr.pryamougolnik(6, 4));
+        // System.out.println(sqr.treugolnik(8, 12));
+        // System.out.println(sqr.krug(6));
+
+        // Task 7
+        // System.out.println("Возраст : " + human.age + " \nИмя : " + human.name);
+        // changeHuman();
+
+        // Task 8
+        // //condition ? value1 : value2
+        // String str = "some string";
+        // boolean a = 2 > 1 ? true : false;
+        // System.out.println(a);
+        // String b = 2 + 2 == 1 + 3 ? "значения равны" : "значения не равны";
+        // System.out.println(b);
+        // boolean c = str.length() > 15 ? true : false;
+        // System.out.println(c);
+        // int hour = 20;
+        // String greeting = hour < 18 ? "Добрый день" : "Добрый вечер";
+        // System.out.println(greeting);
+        // // и т.д. ;)
+
+        // Task 8
+        String str = " Some String ";
+        int x = str.length();
+        System.out.println(x);
+
+        str = str.trim();
+        System.out.println(str);
+
+        char chr = str.charAt(2);
+        System.out.println(chr);
+
+        str = str.concat(" stringed");
+        System.out.println(str);
+
+        str = str.toUpperCase();
+        System.out.println(str);
+
+        str = str.toLowerCase();
+        System.out.println(str);
+
+
+        strbfr.append("some stringBuffer");
+        System.out.println(strbfr);
+
+        strbfr.replace(0, 5, "");
+        System.out.println(strbfr);
+
+        strbfr.reverse();
+        System.out.println(strbfr);
+
+        strbfr.append(" olleH");
+        System.out.println(strbfr);
+
+        strbfr.reverse();
+        System.out.println(strbfr);
+
     }
 
     public static void printArray(int[] array) {
@@ -123,6 +171,12 @@ public class App {
         }
         System.out.println(sum);
         System.out.println(proizv);
+    }
+
+    public static void changeHuman() {
+        human.setAge(21);
+        human.setName("Anatoliy");
+        System.out.println("Возраст : " + human.age + " \nИмя : " + human.name);
     }
 
 }
